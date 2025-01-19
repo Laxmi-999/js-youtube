24857f427070465b0b3289b2bcb381581c812c6a
Laxmi-999 <139095919+Laxmi-999@users.noreply.github.com>
2025-01-19 11:34:56 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,11 @@
+const accountId = 1122;
 
+let emailId = "lax@gmail.com";
+
+var cityId = "3555";
+
+ emailId = "sita@gmail.com";
+
+console.log(accountId);
+
+console.table(["accountId", "cityId"]);
