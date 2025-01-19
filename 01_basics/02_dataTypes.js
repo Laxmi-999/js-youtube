a732e15133c85f51844658cf172c5e5f28b6daa5
Laxmi-999 <139095919+Laxmi-999@users.noreply.github.com>
2025-01-19 15:40:56 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use stric" // treat all JS code as newer version
+ 
+// alert(4+4) only runs on the browser because in browser JS engine is already set
+
+// code readability
+
+// tc39 and mdn
+// ECHMA script is standard language 
+// datatypes
+
+let name = "laxu" // string
+let age = 20 // integer
+let isLoggedIn = false // boolean
+ let state;(undefined)
+
+// number  => 2 to power 53(in range)
+//bigint
+//string => ""
+// boolean = true/false
+//null => standalone value (representation of empty value)// null is one object
+// undefined
+//symbol (to find uniqueness it is usefull)
+
+
+//object
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof "laxu")
+console.log(typeof 8)
+
+
+
