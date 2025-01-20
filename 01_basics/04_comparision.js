97343dc2589e8d00a3e5d80d6281d3d5f6c10911
Laxmi-999 <139095919+Laxmi-999@users.noreply.github.com>
2025-01-20 09:34:54 +0000

    comparisons

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,35 @@
+// Basic and simple comaparions
+
+console.log(1 > 3);
+console.log(2 <= 4);
+console.log(4 != 8);
+console.log(4 == 2+2);
+
+// camparisons between different datatypes
+
+console.log("2" > 1);
+console.log("04" < 5);
+
+//complex, conflicting and confusing comparisions in JS
+
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+// stric comparisons
+
+console.log("2" === 2); // "==" this comapars by conversing datatypes and "===" this compars without conversing 
+
+
+
+
+
+
+
+
+
