4b04989224ebc82ff9e97f54ad01f22f76f57eb1
Laxmi-999 <139095919+Laxmi-999@users.noreply.github.com>
2025-01-20 10:13:56 +0000

    datatypes summary

---
+++ 01_basics/datatypesSummary.js
@@ -0,0 +1,41 @@
+//Primitive datatypes ==> predefined by the language and named by reserved keywords
+
+// 7 types of primitive datatypes in JS. They are:
+// string, number, boolean, null, undefined, symbol,bigInt
+
+const name = "lax" // string
+const age = 23 // number
+const isLoggedIn = true // boolean
+const temp = null // null or empty
+let email; // reserved memory space bt value is undefinded
+const id = Symbol('333');
+const anotherId = Symbol('333');
+
+const bigNum = 794383573238695n;
+
+console.log(id === anotherId);
+console.log(id);
+
+
+// Reference types or NON-Primitive datatypes
+//Array, object , function
+
+const  persons = ["sita", "ram", "hari"] // array
+
+const details= {
+    name:"sita",
+    age: "23"
+
+}
+
+const myFunction = function(){
+
+    console.log("this is my function");
+
+}
+myFunction();
+console.log(typeof persons);
+console.log(typeof details);
+console.log(typeof myFunction);
+
+
