f4989370970c57f37b12ae8125cc657c75182870
Laxmi-999 <139095919+Laxmi-999@users.noreply.github.com>
2025-01-20 08:53:36 +0000

    operations done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,7 +29,48 @@ let someNumber = 77
 
 let stringNumber = String(someNumber);
 
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);f
+// console.log(stringNumber);
+
+/*----------------------OPERATIONS IN JS--------------------------------------------*/
+
+let str1 = "hi";
+let str2= " lax";
+
+let str3 = str1 + str2 ;
+console.log(str3);
+
+console.log("1" +6);
+console.log(1 +"6");
+console.log("1" +6+5);
+console.log(1 +6+"6");
+
+
+// tricky conversions//
+console.log(true);
+console.log(+true);
+// console.log(true+);
+console.log(+ "");
+
+
+
+// console.log(3+4*7%3); // not appriciated use paranthesis
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2;
+
+
+let gameCounter = 100;
+++gameCounter; // prefix operator
+gameCounter++; // postfix operator
+console.log(gameCounter);
+
+
+
+
+
+
+
 
 
