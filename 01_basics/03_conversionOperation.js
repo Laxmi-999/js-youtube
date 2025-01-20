fc0a20fac70c63d3ce6648c3079bb5898e683ef9
Laxmi-999 <139095919+Laxmi-999@users.noreply.github.com>
2025-01-20 04:32:59 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = "laxmi";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+
+// "33" => 33
+// "22abc" => NaN
+// true =>1 , false =>0
+
+let isLoggedIn = "";
+
+console.log(typeof isLoggedIn);
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+//NOTES
+//1 => ture,
+//0 ==> false,
+//"laxu" => true,
+//"" => false,
+
+let someNumber = 77
+
+let stringNumber = String(someNumber);
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+
