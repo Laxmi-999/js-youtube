c937d38308ac70e805652b66e2217c377153f51e
Laxmi-999 <139095919+Laxmi-999@users.noreply.github.com>
2025-01-24 09:42:57 +0000

    nested function and conditons

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -18,3 +18,47 @@ if(true){
 console.log("outer A",a);
 console.log("outer B",b);
 console.log("outer C", c);
+
+
+/// nested functions concept
+
+function one(){  // here one is parent function and two is child function
+    return userName = "laxmi";
+
+    function two(){
+        return website = "laxmi.com";
+        console.log("user is ", userName);  // child functions can access the properties of parent function
+        
+    }
+    // console.log("website is ", website); // parent function can not access the property of child function
+    console.log("i am the child function", two());
+}
+console.log(" i am the parent function ", one());
+
+
+
+//// nested if else 
+
+if(true){
+    const userName = "vicky"
+
+    if(userName){
+        const website =" youtube"
+        console.log(userName + website);
+        
+    }
+   console.log(userName); 
+}
+
+addOne(8) // in this u can use function call before function initialization 
+function addOne(num){ /// this is basic function
+    return num+2
+}
+console.log(addOne(5));
+
+
+// addTwo(6)  this is case u can not use function call before function initializations;// this case is called hosting
+const addTwo = function(num) { // another method to declare function,, it is called expression where variable holds functions, and variables are very powerfull in javascript it can hold any type of data= like array, object, functions and more.
+    return num +2;
+}
+console.log(addTwo(4));
