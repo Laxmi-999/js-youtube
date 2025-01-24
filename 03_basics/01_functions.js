8aeb9655ded6dd9ab62c3bdd833d3ae7e9c8a534
Laxmi-999 <139095919+Laxmi-999@users.noreply.github.com>
2025-01-24 08:40:31 +0000

    object_arrray and function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -42,3 +42,40 @@ console.log(loggedOutUser("laxmi"));
 
 
 
+// passing object to the function
+
+
+
+function calculateCartPrice(value1,value2,...numb) // if u have to pass multiple value as a single argumment we use rest operator ...
+{
+
+    return numb
+
+}
+console.log(calculateCartPrice(300,500,400,200));
+
+////
+const user = {
+    name:"syam",
+   price:"400"
+}
+
+function handleObject(anyobject) {
+    
+    console.log(`username is ${anyobject.name} and price is ${anyobject.price}`);
+    
+}
+handleObject(user);
+
+
+//// passing array to the function 
+
+const myArray =[400,500,200,600]
+
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+console.log(returnSecondValue(myArray));
+console.log(returnSecondValue([400,500,200,600]));
+
+
